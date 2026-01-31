768e39ef3870192022e2c1c1361543d3fb8e57df
southwestmogrown <southwestmogrown@gmail.com>
2026-01-31 05:34:11 -0600

    refactor for modularity

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 
+load_dotenv()
 
-def main():
-    load_dotenv()
-    key = os.environ.get("GEMINI_API_KEY")
+def get_and_check_key(key_name):
+    key = os.environ.get(key_name)
+    if key == None:
+        raise RuntimeError("API key not found...")
+    return key
+
+def parse_user_prompt():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User Prompt")
     prompt = parser.parse_args()
+    return prompt
 
-    if key == None:
-        raise RuntimeError("API key not found...")
-
+def prompt_client(prompt, key):
     client = genai.Client(api_key=key)
-
     res = client.models.generate_content(model="gemini-2.5-flash", contents=prompt.user_prompt)
 
     if res.usage_metadata == None:
@@ -24,6 +27,13 @@ def main():
     print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
     print(res.text)
+
+
+def main():
+    user_prompt = parse_user_prompt()
+    gemini_key = get_and_check_key("GEMINI_API_KEY")
+
+    prompt_client(user_prompt, gemini_key)
 
 
 if __name__ == "__main__":
