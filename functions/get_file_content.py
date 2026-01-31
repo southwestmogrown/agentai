cb11bf8066aea418b4fda840e28f8663e8cdf4ad
southwestmogrown <southwestmogrown@gmail.com>
2026-01-31 15:18:43 -0600

    added file read function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,15 +1,30 @@
 import os
 
+from config import *
+
 def get_file_content(working_directory, file_path):
-  working_dir_abs = os.path.abspath(working_directory)
-  target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+  try:
+    working_dir_abs = os.path.abspath(working_directory)
+    target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
 
-  # Will be True or False
-  valid_target_file = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+    # Will be True or False
+    valid_target_file = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
 
-  if valid_target_file == False:
-    return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-  
-  if not os.path.isfile(target_file):
-    return f'Error: File not found or is not a regular file: "{file_path}"'
+    if valid_target_file == False:
+      return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(target_file):
+      return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    with open(target_file, 'r') as f:
+      content = f.read(MAX_CHARS)
+
+      if f.read(1):
+        content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+      return content
+    
+  except BaseException as e:
+    return f"Error: The following error has occured...\n {e}"
+
   
